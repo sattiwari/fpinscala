cc55a119ad62d6637fd81a7d86471098a71d9565
Satendra Tiwari <sattiwari18@gmail.com>
2017-04-21 09:24:39 +0700

    ex01 complete

---
+++ src/main/scala/errorhandling/Option.scala
@@ -0,0 +1,48 @@
+package errorhandling
+
+// hide std lib Option & Either, since we are writing our own
+import scala.{Option => _, Either => _,_}
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = {
+    this match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
+  }
+
+  def getOrElse[B>:A](default: => B): B = {
+    this match {
+      case None => default
+      case Some(a) => a
+    }
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if(f(a)) this else None)
+
+}
+
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object OptionOps extends App {
+
+  var x = Option(4)
+  var x2 = Option(Option(4))
+  var y = None
+
+//  println(x.map(t => t+1))
+//  println(x.getOrElse(1))
+//  println(y.getOrElse(1))
+//    println(x.orElse(Option(1)))
+//  println(y.orElse(None))
+//  println(x.flatMap(t => Option(t+1)))
+  println(x.filter(t => t %2 == 0))
+  println(x.filter(t => t %2 != 0))
+
+
+}
